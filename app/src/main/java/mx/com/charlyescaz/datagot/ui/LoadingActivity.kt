d75841d4600886409a101ecafd3360bdc4307a61
Carlos Escobar <carlos.satoritech@gmail.com>
2021-03-07 21:21:04 -0600

    :panda_face: Laoding Activity

---
+++ app/src/main/java/mx/com/charlyescaz/datagot/ui/LoadingActivity.kt
@@ -0,0 +1,33 @@
+package mx.com.charlyescaz.datagot.ui
+
+import android.content.Intent
+import android.net.sip.SipErrorCode.TIME_OUT
+import android.os.Bundle
+import android.os.Handler
+import androidx.databinding.DataBindingUtil
+import mx.com.charlyescaz.datagot.R
+import mx.com.charlyescaz.datagot.databinding.ActivityLoadingBinding
+import mx.com.charlyescaz.datagot.ui.home.HomeActivity
+
+
+class LoadingActivity: CustomActivity() {
+
+    private lateinit var vBind: ActivityLoadingBinding
+    private val TIME_OUT = 3000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        vBind = DataBindingUtil.setContentView(this, R.layout.activity_loading)
+        setupNoNavigation()
+        setupDelay()
+    }
+
+    fun setupDelay(){
+        Handler().postDelayed(Runnable {
+            val i = Intent(this, HomeActivity::class.java)
+            startActivity(i)
+            finish()
+        }, TIME_OUT)
+    }
+}
